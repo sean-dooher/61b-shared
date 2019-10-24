07c44b487cf1913b8b070b80d6b5b848cfc796b6
Sean Dooher <sean@dooher.net>
2019-10-24 10:28:39 -0700

    Update hw6

---
+++ hw6/ECHashStringSet.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/** A set of String values.
+ *  @author
+ */
+class ECHashStringSet implements StringSet {
+    @Override
+    public void put(String s) {
+        
+    }
+
+    @Override
+    public boolean contains(String s) {
+        return false;
+    }
+
+    @Override
+    public List<String> asList() {
+        return null; //FIXME
+    }
+}
---
+++ hw6/HW6Test.java
@@ -0,0 +1,11 @@
+/** Tests of HW5
+ *  @author P. N. Hilfinger
+ */
+public class HW6Test {
+
+    public static void main(String[] args) {
+        System.exit(ucb.junit.textui.runClasses(BSTStringSetTest.class, BSTStringSetRangeTest.class, ECHashStringSetTest.class));
+    }
+
+}
+
